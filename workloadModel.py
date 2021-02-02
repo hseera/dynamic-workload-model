e486c773812e6dc6d6d37197654949229c6c8f2f
hseera <harinderseera@gmail.com>
2021-02-02 19:05:35 +1100

    Update workloadModel.py
    updated the workload model script but adding a function to generate sustain workload model

--- workloadModel.py
+++ workloadModel.py
@@ -1,16 +1,39 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 04 11:46:53 2020
-
-@author: harinder
-"""
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''Default parameter values for the dynamic workload.
+Update them to generate the right workload model.
+'''
+DURATION = 3600
+Y1_AMPLITUDE = 25
+Y2_AMPLITUDE = 25
+Y1_NO_OF_CYCLES = 12
+Y2_NO_OF_CYCLES = 4
+Y1_HORIZONTAL_SHIFT = 0
+Y2_HORIZONTAL_SHIFT = 0
+Y1_VERTICAL_SHIFT = 25
+Y2_VERTICAL_SHIFT = 25
 
-#using two cosine wave functions to generate a workload model
+'''Default values for a sustain load model.
+Update them to with the correct rampu and rampdown duration
+'''
+RAMPUP = 400
+RAMPDOWN = 400
+TIME=np.linspace(0,DURATION,DURATION)
+
+
+#step function
+def linear_step_func(x,x0,x1):
+    y= np.piecewise(x, [
+        x < x0, 
+       (x >= x0) & (x <= x1), 
+        x > x1],
+            [0.,
+            lambda x: x/(x1-x0)+x0/(x0-x1), 1.])
+    return y
+
+#Using two cosine wave functions to generate a dynamic workload model
 def workload_function(T, y1Amplitude, y2Amplitude, y1NoofCycles, y2NoofCycles, y1HozShift, y2HozShift, y1VerShift, y2VerShift):
     workloadModel = []
     x = np.arange(0,T)
@@ -19,31 +42,33 @@ def workload_function(T, y1Amplitude, y2Amplitude, y1NoofCycles, y2NoofCycles, y
     workloadModel.append((x, y1+y2))
     return workloadModel
 
-def main():
-    #update the below default variable values to generate the correct workload model as per your requirement.
-    duration = 3600
-    y1Amplitude = 25
-    y2Amplitude = 25
-    y1NoofCycles = 12
-    y2NoofCycles = 4
-    y1HorizontalShift = 0
-    y2HorizontalShift = 0
-    y1VerticalShift = 25
-    y2VerticalShift = 25
 
+#function to generate data for the sustain workload model
+def sustain_function(rampup,rampdown,duration, time):
+    workloadModel = []
+    workloadModel.append((time,(linear_step_func(time,0,rampup)+linear_step_func(time,duration-rampdown,duration)*-1)*100))
+    return workloadModel
+
+def main():
     
-    model = workload_function(duration,y1Amplitude,y2Amplitude,y1NoofCycles,y2NoofCycles,y1HorizontalShift,y2HorizontalShift,y1VerticalShift,y2VerticalShift)
+    #Generate a dynamic workload model
+    model = workload_function(DURATION,Y1_AMPLITUDE,Y2_AMPLITUDE,Y1_NO_OF_CYCLES,
+                Y2_NO_OF_CYCLES,Y1_HORIZONTAL_SHIFT,Y2_HORIZONTAL_SHIFT,Y1_VERTICAL_SHIFT,Y2_VERTICAL_SHIFT)
     
-    '''Using below to plot a graph to show what kind of workload model the above code generates with default function values. You can instead covert this function into an api call which you can then call in your load test tool. The api will return
-    a list of values which you can parse in your load test tool to emulate the workload model.
+    #Generate a sustain workload model with rampup and rampdown
+    #model = sustain_function(RAMPUP,RAMPDOWN,DURATION,TIME)
+    
+    '''Using below to plot a graph to show what kind of workload model the above code generates with default function values. 
+    You can instead covert this function into an api call which you can then call in your load test tool. 
+    The api will return a list of values which you can parse in your load test tool to emulate the workload model.
     '''
-    
     x_val = [x[0] for x in model]
     y_val = [x[1] for x in model]
     
     plt.plot(x_val,y_val)
     plt.plot(x_val,y_val,'-_r')
     plt.show()
+
 if __name__ == "__main__":
     main()
     
